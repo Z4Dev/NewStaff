d451edd43b7fe8763f90ac62e371d0586c8e474e
Z4 DEV <z4kreyyt@gmail.com>
2023-10-15 01:51:34 +0100

    🎉 First Commit

---
+++ src/main/java/pt/z4/newstaff/commands/nstaff.java
@@ -0,0 +1,12 @@
+package pt.z4.newstaff.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class nstaff implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
