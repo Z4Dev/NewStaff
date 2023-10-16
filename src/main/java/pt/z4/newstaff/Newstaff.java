18f15958b2e183ab233c11b744c3c19694d61636
Z4 DEV <z4kreyyt@gmail.com>
2023-10-17 00:40:02 +0100

    ✨ Adding Pickup + Drop + Inventory Click Events

--- src/main/java/pt/z4/newstaff/Newstaff.java
+++ src/main/java/pt/z4/newstaff/Newstaff.java
@@ -1,11 +1,15 @@
 package pt.z4.newstaff;
 
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import pt.z4.newstaff.Database.DatabaseManager;
 import pt.z4.newstaff.Database.tables.StaffTable;
 import pt.z4.newstaff.commands.nStaff;
 import pt.z4.newstaff.commands.nStaffChat;
 import pt.z4.newstaff.commands.nTpTo;
+import pt.z4.newstaff.events.PlayerDrop;
+import pt.z4.newstaff.events.PlayerInventoryClick;
+import pt.z4.newstaff.events.PlayerPickup;
 
 public final class Newstaff extends JavaPlugin {
 
@@ -14,10 +18,17 @@ public final class Newstaff extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
         saveDefaultConfig();
         databaseManager = new DatabaseManager(this);
         staffTable = new StaffTable(databaseManager);
         staffTable.createTables();
+
+        PluginManager pluginManager = getServer().getPluginManager();
+
+        pluginManager.registerEvents(new PlayerDrop(staffTable), this);
+        pluginManager.registerEvents(new PlayerPickup(staffTable), this);
+        pluginManager.registerEvents(new PlayerInventoryClick(staffTable), this);
 
         getCommand("nstaff").setExecutor(new nStaff(staffTable));
         getCommand("staffchat").setExecutor(new nStaffChat());
